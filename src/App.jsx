3862caa96a4abde1be742e998bc6a3e649234fa0
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2023-12-17 06:22:58 -0800

    make site for documentations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import './App.css'
 
 function App() {
+    return(
+        
+        <div>
+            <h1>Light Mode</h1>
+            <p className='text'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo ipsum commodi nisi 
+                suscipit ad qui quia quos quis? Facere nam, quo incidunt qui explicabo atque pariatur 
+                molestiae recusandae. Fugiat, assumenda.
+            </p>
+            <button><p className='currentmode'>Dark Mode</p></button>
+        </div>
+    )
   
 }
 

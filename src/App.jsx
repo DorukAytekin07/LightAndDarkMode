59b5f711d84d01c6adcff8c599bd384ac705af2d
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2023-12-17 06:25:26 -0800

    make site for documentations

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ function App() {
             <p className='text'>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo ipsum commodi nisi 
                 suscipit ad qui quia quos quis? Facere nam, quo incidunt qui explicabo atque pariatur 
-                molestiae recusandae. Fugiat, assumenda.
+                molestiae recusandae. Fugiat, assumenda. Lorem ipsum dolor sit amet, 
+                consectetur adipisicing elit. Accusantium suscipit voluptas numquam error 
+                consequuntur quis ipsam dolorum libero eaque laudantium, ex reprehenderit 
+                explicabo quos ea omnis. Praesentium assumenda voluptatibus dolor.
             </p>
             <button><p className='currentmode'>Dark Mode</p></button>
         </div>
